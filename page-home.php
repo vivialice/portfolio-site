6fd2b6aeedfab2a237df01483259cd00e2ae3700
Vivien Ilett <vivien.ilett@gmail.com>
2015-03-12 15:49:10 -0400

    updates

--- page-home.php
+++ page-home.php
@@ -7,6 +7,12 @@
 get_header();  ?>
 
 <div class="main">
+
+	<section class="landing">
+		
+		
+	</section><!-- /.landing -->
+	
 	<div class="container">
 		
 		<div class="aboutContainer clearfix">
@@ -22,35 +28,42 @@ get_header();  ?>
 					<a href="#">Say Hello</a>
 				</div><!-- /.contactBtn -->
 			</div><!-- /.aboutRight -->
+
+			<!-- <h2>Selected Work</h2> -->
 		</div><!-- /.aboutContainer -->
 
 	</div> <!-- /.container -->
 
-    <?php // Start the loop ?>
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<section class="portfolio">
+	    <?php // Start the loop ?>
+	    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-      <?php the_content(); ?>
+	      <?php the_content(); ?>
 
-    <?php endwhile; // end the loop?>
+	    <?php endwhile; // end the loop?>
 
-    <?php // pull 6 portfolio pieces ?>
-	<?php $latestPosts = new WP_Query(array(
-		'post_type' => 'portfolio', 
-		'posts_per_page' => 6
-	)); ?>
+	    <?php // pull 6 portfolio pieces ?>
+		<?php $latestPosts = new WP_Query(array(
+			'post_type' => 'portfolio', 
+			'posts_per_page' => 6
+		)); ?>
 
-	<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post(); ?>
-			<a href="<?php echo get_permalink(); ?>">
-				<div class="portfolioContent">
-					<h2><?php the_title(); ?></h2>
-					<h3><?php the_field('description'); ?></h3>
-					<?php the_terms($post->ID,'technical_skills', '', ''); ?>
-				</div><!-- /.portfolioContent -->
-					<?php the_post_thumbnail( 'homepage-thumb' ); ?>
-			</a>
+		<?php if($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post(); ?>
+					<div class="portfolioSq">
+						<div class="portfolioContent"><a class="box" href="<?php echo get_permalink(); ?>">
+							<h2><?php the_title(); ?></h2>
+							<h3><?php the_field('description'); ?></h3>
+							<div class="tag">
+								<?php the_terms($post->ID,'technical_skills', '', ''); ?>
+							</div><!-- /.tag -->
+						</div><!-- /.portfolioContent -->
+							<?php the_post_thumbnail( 'homepage-thumb' ); ?>
+					</a>
+					</div><!-- /.portfolioSq -->
 
-	<?php endwhile // end custom loop ?>
-	<?php wp_reset_postdata(); // return end, must do on all custom queries ?>
+		<?php endwhile // end custom loop ?>
+		<?php wp_reset_postdata(); // return end, must do on all custom queries ?>
+	</section><!-- /.portfolio -->
 	
 	<div class="container">
 		<?php echo do_shortcode( get_field('contact_form') ); ?>
